267be98834455d66f9892c632d1713f3483f3317
Aaron Toponce <aaron.toponce@gmail.com>
2014-06-18 10:12:09 -0600

    Updates from Guilhem Moulin <guilhem@guilhem.org>
    party.sh: replace bashisms by POSIX compliant constructions.
    party-worksheet: Show keyart's multikey handling.

--- doc/examples/party.sh
+++ doc/examples/party.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-FILE="art.txt"
-LEN="$(wc -l $FILE | awk '{print $1}')"
+set -ue
+
+FILE="${1:-art.txt}"
+LEN=$(wc -l <"$FILE")
 I=1
 
-while [[ $I -le $LEN ]]; do
-    LINE1=$(sed -n "$((I+00))p" $FILE)
-    LINE2=$(sed -n "$((I+13))p" $FILE)
-    LINE3=$(sed -n "$((I+26))p" $FILE)
-    LINE4=$(sed -n "$((I+39))p" $FILE)
-    LINE5=$(sed -n "$((I+52))p" $FILE)
+while [ $I -le $LEN ]; do
+    LINE1="$(sed -n "$((I+00))p" "$FILE")"
+    LINE2="$(sed -n "$((I+13))p" "$FILE")"
+    LINE3="$(sed -n "$((I+26))p" "$FILE")"
+    LINE4="$(sed -n "$((I+39))p" "$FILE")"
+    LINE5="$(sed -n "$((I+52))p" "$FILE")"
     echo "${LINE1}${LINE2}${LINE3}${LINE4}${LINE5}"
-    if [ $((I%13)) -eq 0 ]; then I=$((I+52)); fi
+    [ $((I%13)) -eq 0 ] && I=$((I+52))
     I=$((I+1))
 done
